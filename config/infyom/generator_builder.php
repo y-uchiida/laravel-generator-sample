c1e65b98013f8bd65b85e876a9845da2d3374caa
yugo uchiida <y.uchiida@nirneege.com>
2023-08-06 20:02:24 +0900

    Generator GUI builder が、公開したview ファイルを参照するようにconfigを変更

--- config/infyom/generator_builder.php
+++ config/infyom/generator_builder.php
@@ -4,9 +4,9 @@ return [
 
     'views' => [
 
-        'builder' => 'generator-builder::builder',
+        'builder' => 'infyom.generator-builder.builder',
 
-        'field-template' => 'generator-builder::field-template',
+        'field-template' => 'infyom.generator-builder.field-template',
 
         'relation-field-template' => 'generator-builder::relation-field-template',
     ],
