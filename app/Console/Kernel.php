d558e90706d3561ac64b36d223b2d016c65b58b1
yugo uchiida <y.uchiida@nirneege.com>
2023-08-06 20:50:36 +0900

    Generator GUI builder からCRUDを作成しようとした際にエラーが出るので、Kernelにコマンドを追加 https://github.com/InfyOmLabs/generator-builder/issues/61

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,22 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use InfyOm\Generator\Commands\API\APIControllerGeneratorCommand;
+use InfyOm\Generator\Commands\API\APIGeneratorCommand;
+use InfyOm\Generator\Commands\API\APIRequestsGeneratorCommand;
+use InfyOm\Generator\Commands\APIScaffoldGeneratorCommand;
+use InfyOm\Generator\Commands\Scaffold\ScaffoldGeneratorCommand;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        APIGeneratorCommand::class,
+        APIRequestsGeneratorCommand::class,
+        APIScaffoldGeneratorCommand::class,
+        ScaffoldGeneratorCommand::class,
+        APIControllerGeneratorCommand::class,
+    ];
+
     /**
      * Define the application's command schedule.
      */
